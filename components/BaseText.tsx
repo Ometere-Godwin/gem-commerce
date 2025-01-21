a7e77e9d6314fad7ead2c43bfa0fc861668966df
Ometere-Godwin <ookuoluse@yahoo.com>
2025-01-21 03:01:14 +0100

    gap added

--- components/BaseText.tsx
+++ components/BaseText.tsx
@@ -6,7 +6,7 @@ interface BaseTextProps {
 
 export default function BaseText({ title, description }: BaseTextProps) {
   return (
-    <div className="flex flex-col md:gap-y-8">
+    <div className="flex flex-col md:gap-y-8 gap-y-8">
       <h1 className="text-2xl md:text-3xl font-bold">{title}</h1>
       <p className="text-sm md:text-sm">{description}</p>
     </div>
